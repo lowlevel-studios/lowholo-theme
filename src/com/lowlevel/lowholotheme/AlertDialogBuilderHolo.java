43a07f7082bf2b40578613dc7bc07e16b287a5ab
Miguel Botón <mboton@gmail.com>
2012-05-12 16:11:36 +0200

    Added Apache license.

--- src/com/lowlevel/lowholotheme/AlertDialogBuilderHolo.java
+++ src/com/lowlevel/lowholotheme/AlertDialogBuilderHolo.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 Lowlevel Studios
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.lowlevel.lowholotheme;
 
 import android.app.AlertDialog;
@@ -6,6 +22,7 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -57,6 +74,10 @@ public class AlertDialogBuilderHolo extends AlertDialogHolo.Builder {
 				negative.setBackgroundResource(R.drawable.button_holo);
 			if (neutral != null)
 				neutral.setBackgroundResource(R.drawable.button_holo);
+			
+			Log.w("uMusic", "Positive: " + positive.getHeight());
+			Log.w("uMusic", "Negative: " + negative.getHeight());
+			Log.w("uMusic", "Neutral: " + neutral.getHeight());
 		}
 		
 		return dialog;
